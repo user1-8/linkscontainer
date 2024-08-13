8f8c4d02987cf930b648216674362765c9295f14
adam18092005 <adam18092005@gmail.com>
2024-08-14 02:27:30 +0530

    first commit

---
+++ links-container.js
@@ -0,0 +1,20 @@
+var links_obj= {
+
+'MyPW': 'https://bit.ly/mypw1',
+
+'Full books': 'https://bit.ly/fullbk1',
+
+'temp for file share': 'https://bit.ly/temp1809',
+
+'Yt plylst: Alternating Current (TB)': 'https://www.youtube.com/playlist?list=PLfWIv20OHtSfpQgs77iEuwFjRzTZpdG3k',
+
+'Yt plylst: EM waves (TB)': 'https://www.youtube.com/playlist?list=PLfWIv20OHtSevcBzF8AOcR7AHOpWBvF1k',
+
+'Yt plylst: Dual Nature of Radiation&Matter (TB)': 'https://www.youtube.com/playlist?list=PLfWIv20OHtSd1HE255uCzwg_HJaIkcgwa',
+
+
+
+
+
+
+};
---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	// console.log(links_obj);
+
+	let initialTemplate = $('.links_container').html();
+
+	$('.links_container').html('No links yet');
+
+	for (x in links_obj) {
+		$('.links_container').append(initialTemplate.replace('Link Title 1', x).replace('https://www.example.com/', links_obj[x]));
+	}
+
+
+	$('.link-card').on('click',function(){
+		let mythis= $(this)
+		window.open($(mythis).children('p').text(), '_blank').focus();
+	});
+
+
+
+
+})
