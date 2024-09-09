3ab04302f679a14aad7dcd2de66ec8b5d70d6121
adam18092005 <adam18092005@gmail.com>
2024-09-10 00:25:56 +0530

    commited

--- links-container.js
+++ links-container.js
@@ -6,11 +6,15 @@ var links_obj= {
 
 'temp for file share': 'https://bit.ly/temp1809',
 
+'plylst: Org. & Population': 'https://www.youtube.com/playlist?list=PLfWIv20OHtSfucYYE05VTi3_COrKZQ960',
+
 'plylst: humn Health & Diseases.': 'https://youtube.com/playlist?list=PLJyab0VQDBGWN8QnBrfxPmaX7WWBvi22V&si=EY4pUWqvM_acUl82',
 
 'plylst: Biotech princip. & proces.': 'https://www.youtube.com/playlist?list=PLfWIv20OHtScs6HxiXrBwqomQpcFfmOxw',
 
 'plylst: D&F(MD) (L3 missing)': 'https://www.youtube.com/playlist?list=PLfWIv20OHtScvufGJZWOAO8ae5ulOyBJU',
+
+'plylst: Wave Optics (TB)': 'https://www.youtube.com/playlist?list=PLfWIv20OHtSe_rkjc0SB-JwYI1GCGCRAm',
 
 'plylst: Ray Optics (TB)': 'https://www.youtube.com/playlist?list=PLfWIv20OHtSf74vtg1q_2sUM8tAaRYNKd',
 
